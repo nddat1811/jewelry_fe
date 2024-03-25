ac9301a91edd0d4c738be1af85676446e113f212
nddat1811 <63857350+nddat1811@users.noreply.github.com>
2024-03-25 07:34:29 +0700

    navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  // throw new Error('Home')
+  return (
+    <div>Hello</div>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    // </main>
+  )
+}
